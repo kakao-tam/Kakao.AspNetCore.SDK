d994daa9f62250c31fb67d685cd587ee17e7e576
mcpeleeGit <mcpelee@gmail.com>
2022-06-14 00:24:19 +0900

    alpha 0.1.2 bugfix

--- Example/Program.cs
+++ Example/Program.cs
@@ -21,14 +21,8 @@ builder.Services.AddSession(options =>
 
 // ※ KakaoSdk 라이브러리는 Kakao.AspNetCore.Authentication.OAuth2 라이브러리와 무관한 라이브러리입니다.
 // ※ Kakao관련 인가요청 주소, 리다이렉트 URI 처리 관련 API, 토큰요청 주소, API 호출 주소 등. 라이브러리 내부 설정되어 있으므로 UseKakaoSDKInit으로 앱키만 설정하면 됩니다.   
-// ※ Redirect URI는 기본값 "/UserApi/KakaoLoginRedirectUri"이고, UseKakaoSDKInit의 파라메터로 재정의해서 커스터마이징 가능합니다.
 // ※ 액세스 토큰은 HttpContext Session을 필수로 사용하며 Session에 kakao_access_token Key로 저장됩니다.
-// ※ Session, kakao_access_token에 유효한 토큰을 설정하면 별도 인가요청 절차 없이 API를 사용할 수 있습니다. 
-builder.Services.UseKakaoSDKInit("앱키","클라이언트 시크릿");
-
-
-
-
+builder.Services.UseKakaoSDKInit("앱키", "클라이언트 시크릿");
 
 
 var app = builder.Build();
@@ -44,7 +38,6 @@ if (app.Environment.IsDevelopment())
 }
 app.MapControllers();
 app.UseSession();
-
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
